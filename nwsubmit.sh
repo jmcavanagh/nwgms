dce62eda4b97c55072ccd6b4a953c683a9bb7806
jmcavanagh <84742469+jmcavanagh@users.noreply.github.com>
2021-10-27 15:31:08 -0400

    Update nwsubmit.sh
    Updated for new mpi on oscar

--- nwsubmit.sh
+++ nwsubmit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-#SBATCH -t 4:00:00
-#SBATCH -n 1
-#SBATCH --mem=2G
-nwchem $1.inp >& $1.out
+#SBATCH -t 24:00:00
+#SBATCH -n 4
+srun --mpi=pmix nwchem $1.inp >& $1.out
+if [ -z "$2" ]
+then
+        rm $1.hess
+        rm $1.b*
+        rm $1.c
+        rm $1.d*
+        rm $1.zmat
+        rm $1.m*
+fi
